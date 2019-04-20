3ad79228afbe1e24f2f93726c285b7a7a72055de
CodingPeaches90 <jordanmay10@hotmail.com>
2019-04-20 02:33:14 +0100

    Adding icon for app launcher

--- app/src/main/java/com/x15515673/smartplant/MainActivity.java
+++ app/src/main/java/com/x15515673/smartplant/MainActivity.java
@@ -3,6 +3,10 @@ package com.x15515673.smartplant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * x15515673 - Jordan May
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
