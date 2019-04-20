0fdc426c231abcb4745bb506abcb3bfa6543c0ec
CodingPeaches90 <jordanmay10@hotmail.com>
2019-04-20 23:27:05 +0100

    More styling, main activity basically done

--- app/src/main/java/com/x15515673/smartplant/MainActivity.java
+++ app/src/main/java/com/x15515673/smartplant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.x15515673.smartplant;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Custom action bar
+        ActionBar actionbar = getSupportActionBar();
+        getSupportActionBar().setDisplayOptions
+                (actionbar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_custom);
+
     }
 }
