03775907091bfb743af4249939b8981c706ce426
CodingPeaches90 <jordanmay10@hotmail.com>
2019-04-24 14:07:32 +0100

    Added Comments and Set Default toggle switch state

--- app/src/main/java/com/x15515673/smartplant/WaterPumpActivity.java
+++ app/src/main/java/com/x15515673/smartplant/WaterPumpActivity.java
@@ -4,17 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
-import com.llollox.androidtoggleswitch.widgets.MultipleToggleSwitch;
 import com.ornach.nobobutton.NoboButton;
-
 import com.llollox.androidtoggleswitch.widgets.ToggleSwitch;
 
-import java.util.List;
-
-import static com.llollox.androidtoggleswitch.widgets.ToggleSwitch.*;
-
 public class WaterPumpActivity extends AppCompatActivity {
+
+    // Declare Variables
 
     NoboButton waterBtn;
     ToggleSwitch seconds;
@@ -26,14 +21,17 @@ public class WaterPumpActivity extends AppCompatActivity {
         setTitle(R.string.activity_name_pump);
         setContentView(R.layout.activity_water_pump);
 
+        // Set final position array
         final int[] possec = new int[1];
 
+        // Assign buttons to resources
         waterBtn = findViewById(R.id.waterbutton);
-
         seconds = findViewById(R.id.matchParentWidthToggleSwitch);
 
-        // toggle
+        // Set Default to 0 for toggle switch
+        seconds.setCheckedPosition(0);
 
+        // Toggle for seconds listener
         seconds.setOnChangeListener(new ToggleSwitch.OnChangeListener() {
             @Override
             public void onToggleSwitchChanged(int position) {
@@ -42,7 +40,7 @@ public class WaterPumpActivity extends AppCompatActivity {
                 possec[0] = position;
             }
         });
-
+        // Button Listener
         waterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
